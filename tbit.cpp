8b17b97a7cf3c2df6e724c068b6d7571f52afa42
wood <wljlinksly@gmail.com>
2019-03-16 21:50:43 +0800

    convert any number to binary bits

---
+++ tbit.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unistd.h>
+#include <cstdio>
+
+void writeBits(const size_t v, int fd)
+{
+    if(!v){
+        std::putchar ('0'); 
+        return; 
+    }
+
+    size_t sz = sizeof(v) * CHAR_BIT;
+    size_t rem = 0;
+    
+    while(sz--){
+        if((rem = v >> sz)){
+            write(fd, (rem & 1) ? "1" : "0", 1);
+        }
+    }
+}
+
+int main(int argc, char** argv){
+    if(argc == 1){
+        std::cout << std::string(argv[0]) << " <number> " << std::endl;
+        exit(0);
+    }
+    int val = std::atoi(argv[1]);
+    writeBits(val, STDOUT_FILENO);
+    std::cout << std::endl;
+}
