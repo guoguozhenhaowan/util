1a389107a2625bbf2d9de452243246137b4dff8d
wood <wljlinksly@gmail.com>
2019-03-18 17:17:01 +0800

    sort

--- gtest.cmd.sh
+++ gtest.cmd.sh
@@ -4,19 +4,19 @@ g++ -std=c++11 -lgtest gtutil.cpp
 g++ -std=c++11 -lgtest gtseq.cpp
 # test read.h
 g++ -std=c++11 -lgtest gtread.cpp read.cpp
+# test fqreader.h
+g++ -std=c++11 -lgtest -lz gtfqreader.cpp fqreader.cpp
+# test writer.h
+g++ -std=c++11 -lz -lgtest gtwriter.cpp writer.cpp fqreader.cpp
 # test nucleotidetree.h
 g++ -std=c++11 -lgtest gtnucleotidetree.cpp nucleotidetree.cpp
 # test overlapanalysis.h
 g++ -std=c++11 -lgtest gtoverlapanalysis.cpp overlapanalysis.cpp
 # test filter.h
 g++ -std=c++11 -lgtest gtfilter.cpp filter.cpp read.cpp options.cpp
-# test fqreader.h
-g++ -std=c++11 -lgtest -lz gtfqreader.cpp fqreader.cpp
-# test writer.h
-g++ -std=c++11 -lz -lgtest gtwriter.cpp writer.cpp fqreader.cpp
 # test evaluator.h
 g++ -std=c++11 -lgtest -lz gtevaluator.cpp evaluator.cpp read.cpp fqreader.cpp nucleotidetree.cpp options.cpp
-# test filterresult.h
-g++ -std=c++11 -lgtest -lz gtfilterresult.cpp filterresult.cpp stats.cpp options.cpp evaluator.cpp nucleotidetree.cpp fqreader.cpp
 # test stats.h
 g++ -std=c++11 -lgtest -lz gtstats.cpp read.cpp stats.cpp options.cpp evaluator.cpp nucleotidetree.cpp fqreader.cpp
+# test filterresult.h
+g++ -std=c++11 -lgtest -lz gtfilterresult.cpp filterresult.cpp stats.cpp options.cpp evaluator.cpp nucleotidetree.cpp fqreader.cpp
