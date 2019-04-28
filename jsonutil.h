4b0bd5c44a2f1334b71307fbc138c137504ac2b7
wood <wljlinksly@gmail.com>
2019-04-28 16:30:23 +0800

    add getCurrentSystemTime function

--- jsonutil.h
+++ jsonutil.h
@@ -16,6 +16,19 @@ namespace jsonutil{
     inline void writeRecord(std::ofstream& ofs, const std::string& padding, const std::string& key, const T& val){
         ofs << padding << "\t\"" << key << "\": " << val << ",\n";
     }
+
+    /** get current system time
+     * @return current system time
+     */
+    inline std::string getCurrentSystemTime(){
+        time_t tt = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        struct tm* ptm = std::localtime(&tt);
+        char date[60] = {0};
+        std::sprintf(date, "%d-%02d-%02d %02d:%02d:%02d",
+                (int)ptm->tm_year + 1900,(int)ptm->tm_mon + 1,(int)ptm->tm_mday,
+                (int)ptm->tm_hour,(int)ptm->tm_min,(int)ptm->tm_sec);
+        return std::string(date);
+    }
 }
 
 #endif
