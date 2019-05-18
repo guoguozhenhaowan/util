edd4e8ba162161f45e2481702bf65d9c10c136dd
wood <wljlinksly@gmail.com>
2019-05-19 01:25:51 +0800

    add strand info and stat utr as well

--- getcds.cpp
+++ getcds.cpp
@@ -8,65 +8,75 @@
 #include "filereader.h"
 #include "filewriter.h"
 
-/** object to store some infomation ofan record of refgene with transcript number added */
 struct ExonRecord{
-    std::vector<std::pair<size_t, size_t>> exoncoord; ///< exon coordinates pair records in genome
-    size_t cdsStart;          ///< CDS starting coordinates in genome
-    size_t cdsEnd;            ///< CDS ending coordinates in genome
-    int firstCDSIndex;        ///< first CDS index in exoncoord
-    int lastCDSIndex;         ///< last CDS index in exoncorod
-    std::string chromeName;   ///< chromesome name of this gene                
-    std::string geneName;     ///< gene name 
-    std::string trsName;      ///< transcript name
-    std::string trsVer;       ///< transcript version
+    std::vector<std::pair<int, int>> exoncoord;
+    int cdsStart = 0;
+    int cdsEnd = 0;
+    int trsStart = 0;
+    int trsEnd = 0;
+    int cdsLen = 0;
+    int geneLen = 0;
+    char strand = '+';
+    int utr3Len = 0;
+    int utr5Len = 0;
+    int firstCDSIndex = 0;
+    int lastCDSIndex = 0;
+    std::string chromeName;
+    std::string geneName;
+    std::string trsName;
+    std::string trsVer;
 
-    /** construct an ExonRecord object */
     ExonRecord() = default;
-    /** destroy an ExonRecord object */
     ~ExonRecord() = default;
-   
-    /** get the cds index in exoncoord and adjust the edging coordinates */
     void truncateByCDS(){
-        firstCDSIndex = 0;
-        lastCDSIndex = 0;
-        for(int i = 0; i < exoncoord.size(); ++i){
+        for(uint32_t i = 0; i < exoncoord.size(); ++i){
+            geneLen += exoncoord[i].second - exoncoord[i].first;
+            if(cdsStart > exoncoord[i].second){
+                utr5Len += exoncoord[i].second - exoncoord[i].first;
+            }
             if(exoncoord[i].first <= cdsStart && exoncoord[i].second >= cdsStart){
+                utr5Len += cdsStart - exoncoord[i].first;
                 exoncoord[i].first = cdsStart;
                 firstCDSIndex = i;
             }
             if(exoncoord[i].first <= cdsEnd && exoncoord[i].second >= cdsEnd){
+                utr3Len += exoncoord[i].second - cdsEnd;
                 exoncoord[i].second = cdsEnd;
                 lastCDSIndex = i;
                 break;
             }
         }
+        for(uint32_t j = lastCDSIndex + 1; j < exoncoord.size(); ++j){
+            geneLen += exoncoord[j].second - exoncoord[j].first;
+            utr3Len += exoncoord[j].second - exoncoord[j].first;
+        }
+        cdsLen = geneLen - utr3Len - utr5Len;
+        if(strand == '-'){
+            std::swap(utr3Len, utr5Len);
+        }
     }
-
-    /** output an exon record to ostream
-     * @param os reference to a ostream
-     * @param rec reference to a ExonRecord object
-     * @return reference to a ostream
-     */
     friend std::ostream& operator<<(std::ostream& os, const ExonRecord& rec){
         if(rec.exoncoord.size() == 0){
             return os;
         }
+        os << "#" << rec.trsName << "\t" << rec.geneLen << "\t" << rec.cdsLen << "\t" << rec.utr5Len << "\t" << rec.utr3Len << "\n";
         os << ">" << rec.geneName << "_" << rec.trsName << "." << rec.trsVer << ",";
         os << rec.chromeName << ":" << rec.cdsStart << "-" << rec.cdsEnd << "\n";
         int i = 0;
-        for(int k = rec.firstCDSIndex; k <= rec.lastCDSIndex; ++k){
-            os << (++i) << "," << rec.exoncoord[k].first << "," << rec.exoncoord[k].second << "\n";
+        if(rec.strand == '+'){
+            for(int k = rec.firstCDSIndex; k <= rec.lastCDSIndex; ++k){
+                os << (++i) << "," << rec.exoncoord[k].first << "," << rec.exoncoord[k].second << "\n";
+            }
+            os << std::endl;
+        }else{
+            for(int k = rec.lastCDSIndex; k >= rec.firstCDSIndex; --k){
+                os << (++i) << "," << rec.exoncoord[k].first << "," << rec.exoncoord[k].second << "\n";
+            }
         }
-        os << std::endl;
         return os;
     }
 };
 
-/** main function to accept arguments
- * @argc number of arguments provided externally
- * @argv array of char* store the externally provided arguments
- * @return 0 if exit successfuly
- */
 int main(int argc, char** argv){
     if(argc < 4){
         std::cout << std::string(argv[0]) << " <refgenewithaccs> <transcriptlist> <output> " << std::endl;
@@ -88,8 +98,11 @@ int main(int argc, char** argv){
         }
         ExonRecord rec;
         rec.trsName = vec[1];
+        rec.trsStart = std::atoi(vec[4].c_str());
+        rec.trsEnd = std::atoi(vec[5].c_str());
         rec.cdsStart = std::atoi(vec[6].c_str());
         rec.cdsEnd = std::atoi(vec[7].c_str());
+        rec.strand = vec[3][0];
         rec.chromeName = vec[2];
         rec.geneName = vec[12];
         rec.trsVer = vec[16];
@@ -97,8 +110,8 @@ int main(int argc, char** argv){
         std::vector<std::string> exonEnd;
         util::split(vec[9], exonStart, ",");
         util::split(vec[10], exonEnd, ",");
-        std::vector<size_t> startCoord;
-        std::vector<size_t> endCoord;
+        std::vector<int> startCoord;
+        std::vector<int> endCoord;
         util::strvec2intvec(exonStart, startCoord);
         util::strvec2intvec(exonEnd, endCoord);
         util::vec2pairvec(startCoord, endCoord, rec.exoncoord);
@@ -109,3 +122,4 @@ int main(int argc, char** argv){
         fwriter.writeString(oss.str());
     }
 }
+
