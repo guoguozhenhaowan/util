95847276ae9460696083cc0306d13a1168cc506a
wood <wljlinksly@gmail.com>
2019-08-05 10:13:30 +0800

    keep expression info

--- anarpt/Venn2Sample.py
+++ anarpt/Venn2Sample.py
@@ -11,20 +11,24 @@ if len(sys.argv) <= 2:
     sys.exit(0)
 anarpt1 = sys.argv[1]
 anarpt2 = sys.argv[2]
-# stat
-dfa = pd.read_excel(anarpt1, sheet_name="AllGeneExp")
-dfb = pd.read_excel(anarpt2, sheet_name="AllGeneExp")
-gena = set(dfa.Gene)
-genb = set(dfb.Gene)
-both = pd.DataFrame({"Gene": list(gena.intersection(genb))})
-onlya = pd.DataFrame({"Gene": list(gena.difference(genb))})
-onlyb = pd.DataFrame({"Gene": list(genb.difference(gena))})
-both.to_csv("Gene.In.Both.csv", index=False)
+# get sample name
 aName = os.path.basename(anarpt1).replace(".report.xlsx", "")
 bName = os.path.basename(anarpt2).replace(".report.xlsx", "")
+# read df
+dfa = pd.read_excel(anarpt1, sheet_name="AllGeneExp")
+dfb = pd.read_excel(anarpt2, sheet_name="AllGeneExp")
+# intersection 
+both = dfa.merge(dfb, how="inner", on="Gene", suffixes=(aName, bName))
+both.to_csv("Gene.In.Both.csv", index=False)
+# only a
+onlya = dfa[~dfa["Gene"].isin(both["Gene"])]
 onlya.to_csv(aName + ".csv", index=False)
+# only b
+onlyb = dfb[~dfb["Gene"].isin(both["Gene"])]
 onlyb.to_csv(bName + ".csv", index=False)
-# plot
+# venn
+gena = set(dfa.Gene)
+genb = set(dfb.Gene)
 plt.figure()
 v = venn2([gena, genb], set_labels=(aName, bName))
 plt.savefig("Venn.png", dpi=300)
